b96ec9200792919f1ff3fa1c5409016f613591d5
Roland <nnamdiigede11@gmail.com>
2021-10-18 07:53:11 +0100

    Implementing a database.

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
-data class Crime(
-    val id: UUID = UUID.randomUUID(), var title: String = "", var date: Date = Date(), var isSolved: Boolean = false, var requiresPolice: Boolean = false)
+@Entity
+data class Crime(@PrimaryKey val id: UUID = UUID.randomUUID(), var title: String = "", var date: Date = Date(), var isSolved: Boolean = false, var requiresPolice: Boolean = false)
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -3,14 +3,6 @@ package com.bignerdranch.android.criminalintent
 import androidx.lifecycle.ViewModel
 
 class CriminalListViewModel : ViewModel() {
-    val crimes = mutableListOf<Crime>()
-    init {
-        for (i in 0 until 100) {
-            val crime = Crime()
-            crime.title = "Crime #$i"
-            crime.isSolved = i % 2 == 0
-            crime.requiresPolice = i % 2 == 0
-            crimes += crime
-        }
-    }
+    private val crimeRepository = CrimeRepository.get()
+    val crimes = crimeRepository.getCrimes()
 }
