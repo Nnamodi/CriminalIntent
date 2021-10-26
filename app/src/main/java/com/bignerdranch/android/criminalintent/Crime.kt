65b78a2558eb3e6c5eb7df0900f423a05fbb3e1b
Roland <nnamdiigede11@gmail.com>
2021-10-26 18:14:00 +0100

    New feature

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -7,4 +7,4 @@ import java.util.*
 @Entity
 data class Crime(@PrimaryKey
                  val id: UUID = UUID.randomUUID(), var title: String = "", var date: Date = Date(),
-                 var isSolved: Boolean = false)
+                 var isSolved: Boolean = false, var requiresPolice: Boolean = false)
